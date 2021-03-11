d846c63995c1a362ac1e5fb3d778ba1ca78021ad
Jacob Keil <jacobkeil13@gmail.com>
2021-03-10 20:11:05 -0500

    auth url

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,15 +9,9 @@ const redirectLogin = (req, res, next) => {
     }
 }
 
-const redirectAuth = (req, res, next) => {
-    res.redirect(
-        "https://discord.com/api/oauth2/authorize?client_id=805015249975640084&redirect_uri=https%3A%2F%2Fcomrades-cove.herokuapp.com%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds"
-        );
-}
+router.get("/discord", passport.authenticate("discord"));
 
-router.get("/discord", redirectAuth);
-
-router.get("/discord/redirect", redirectAuth,(req, res) => {
+router.get("/discord/redirect", passport.authenticate("discord"),(req, res) => {
     res.redirect("/home");
 });
 
