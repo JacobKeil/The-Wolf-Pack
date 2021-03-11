63d96fa6f271d9055195970e234598b749c8fb68
Jacob Keil <jacobkeil13@gmail.com>
2021-03-10 19:51:28 -0500

    auth url

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,6 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-router.get("/discord", passport.authenticate("discord"));
-
-router.get("/discord/redirect", passport.authenticate("discord"),(req, res) => {
-    res.redirect("/home");
-});
-
 const redirectLogin = (req, res, next) => {
     if(!req.user) {
       res.redirect('/auth/discord');
@@ -14,6 +8,18 @@ const redirectLogin = (req, res, next) => {
       next(); 
     }
 }
+
+const redirectAuth = (req, res, next) => {
+    res.redirect(
+        "https://discord.com/api/oauth2/authorize?client_id=805015249975640084&redirect_uri=https%3A%2F%2Fcomrades-cove.herokuapp.com%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds"
+        );
+}
+
+router.get("/discord", redirectAuth);
+
+router.get("/discord/redirect", redirectAuth,(req, res) => {
+    res.redirect("/home");
+});
 
 router.get("/", redirectLogin, (req, res) => {
     if(req.user) {
