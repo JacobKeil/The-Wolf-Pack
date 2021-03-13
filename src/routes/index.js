d8e6e117c82a61d730753dd249b95638f7479506
Jacob Keil <jacobkeil13@gmail.com>
2021-03-13 16:32:25 -0500

    donation commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const router = require("express").Router();
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
 const auth = require("./auth");
 const discord = require("./discord");
 
@@ -63,6 +66,43 @@ router.get("/home", redirectLogin, (req, res) => {
         guilds: req.user.guilds,
         id: req.user.discordId
     });
+});
+
+router.get("/donate", redirectLogin, (req, res) => {
+  //console.log(req.user.avatar);
+  let profilePic = "";
+  if (req.user.avatar == null) {
+    profilePic = "images/default.png";
+  } else {
+    profilePic = `https://cdn.discordapp.com/avatars/${req.user.discordId}/${req.user.avatar}`;
+  }
+  const un = req.user.discordTag.split("#");
+  res.render("donate.ejs", {
+      username: un[0],
+      email: req.user.email,
+      avatar: `<img id="user-logo" src="${profilePic}">`,
+      guilds: req.user.guilds,
+      id: req.user.discordId
+  });
+});
+
+router.get("/donate/send", redirectLogin, (req, res) => {
+  const whurl = process.env.DONATION_DISCORD_WH;
+  const donateHook = new Webhook(whurl);
+  const un = req.user.discordTag.split("#");
+
+  const donateEmbed = new MessageBuilder()
+    .setTitle(`Thank you ${un[0]}`)
+    .setDescription(`Donation was sent by <@${req.user.discordId}>`)
+    .setTimestamp();
+
+  donateHook.send(donateEmbed);
+
+  res.redirect("/donate/thankyou");
+});
+
+router.get("/donate/thankyou", redirectLogin, (req, res) => {
+  res.send("Thank you!")
 });
 
 module.exports = router;
