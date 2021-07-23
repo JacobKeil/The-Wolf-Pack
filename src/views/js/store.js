5404e27ce901fa7bb9a825c4efe09e36f57738ea
Jacob Keil <jacobkeil13@gmail.com>
2021-07-23 15:25:12 -0400

    asd

--- src/views/js/store.js
+++ src/views/js/store.js
@@ -1,6 +1,8 @@
 function spawnItem(item, credits) {
     fetch(`/store?object=${item}&quantity=1&cost=${credits}`, {
         method: "POST"
+    }).then(res => {
+        console.log(res);
     }).catch((error) => {
         console.error(error);
         return;
