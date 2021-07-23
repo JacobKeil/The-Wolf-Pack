1ac6de4e0beecfe1b924755b497c16007a67e3e3
Jacob Keil <jacobkeil13@gmail.com>
2021-07-23 14:59:17 -0400

    asd

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Authorization");
     next();
 });
 
