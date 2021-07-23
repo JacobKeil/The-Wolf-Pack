4216edbcac2a671225b2a782d574817e372d766d
Jacob Keil <jacobkeil13@gmail.com>
2021-07-23 17:48:55 -0400

    xmlhttp

--- src/views/js/store.js
+++ src/views/js/store.js
@@ -1,10 +1,19 @@
+var getUrl = window.location;
+var baseUrl = getUrl.protocol + "//" + getUrl.host;
+
 async function spawnItem(item, credits) {
-    await fetch(`https://the-wolfpack.herokuapp.com/store?object=${item}&quantity=1&cost=${credits}`, {
-        method: "POST"
-    }).then(res => {
-        res.status(200);
-    }).catch((error) => {
-        console.error(error);
-        return;
-    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", `${baseUrl}/store?object=${item}&quantity=1&cost=${credits}`, true);
+    xhr.send();
 }
+
+// async function spawnItem(item, credits) {
+//     await fetch(`https://the-wolfpack.herokuapp.com/store?object=${item}&quantity=1&cost=${credits}`, {
+//         method: "POST"
+//     }).then(res => {
+//         res.status(200);
+//     }).catch((error) => {
+//         console.error(error);
+//         return;
+//     });
+// }
