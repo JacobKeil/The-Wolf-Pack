95f0160e325fb815050f4d12d526895f05f815b5
Jacob Keil <jacobkeil13@gmail.com>
2021-07-23 16:35:22 -0400

    asad

--- src/views/js/store.js
+++ src/views/js/store.js
@@ -2,7 +2,7 @@ async function spawnItem(item, credits) {
     await fetch(`/store?object=${item}&quantity=1&cost=${credits}`, {
         method: "POST"
     }).then(res => {
-        console.log(res);
+        res.status(200);
     }).catch((error) => {
         console.error(error);
         return;
