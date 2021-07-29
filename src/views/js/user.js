971c59f9b2d387ee5542ecf8efa04519ba33ecc9
Jacob Keil <jacobkeil13@gmail.com>
2021-07-29 02:21:00 -0400

    added new tab on steam link

--- src/views/js/user.js
+++ src/views/js/user.js
@@ -3,7 +3,7 @@ let steam_input = document.getElementById("steam-input");
 
 function openSteamModal(steamId) {
     if (steamId === "No Steam ID set") {
-        document.getElementById("steam-title").innerHTML = `Add your Steam ID, click <a href="https://store.steampowered.com/account">here</a> to find it!`;
+        document.getElementById("steam-title").innerHTML = `Add your Steam ID, click <a href="https://store.steampowered.com/account" target="_blank">here</a> to find it!`;
     } else {
         document.getElementById("steam-title").innerHTML = `Update your steam ID if needed, it is currently '${steamId}'`;
     }
