ab50f54b27eb066c0e6aabbdcbf49903c456fc84
Jacob Keil <jacobkeil13@gmail.com>
2021-08-07 14:49:44 -0400

    removing unneeded comments

--- src/views/js/home.js
+++ src/views/js/home.js
@@ -24,37 +24,25 @@ var baseUrl = getUrl.protocol + "//" + getUrl.host;
 
 closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
-  menuBtnChange();//calling the function(optional)
+  menuBtnChange();
 });
 
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+searchBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
+  menuBtnChange();
 });
 
-// following are the code to change sidebar button(optional)
 function menuBtnChange() {
  if(sidebar.classList.contains("open")){
-   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
  }else {
-   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
  }
 }
 
 function changeValues() {
     total.value = "$" + priceAmount.value;
 }
-
-// function donateMoney() {
-//     if (total == "") {
-//         return;
-//     }
-//     console.log("passed stripe handler");
-//     const Http = new XMLHttpRequest();
-//     const url = `${baseUrl}/home/donate?price=${total.value.replace("$", "")}`;
-//     Http.open("POST", url, true);
-//     Http.send();
-// }
 
 function donateClicked(price_id) {
     fetch(`/home/donate/charge?price=${price_id}`, {
