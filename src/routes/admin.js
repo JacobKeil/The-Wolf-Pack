d03da30852cedaa0b55895561e43f2fa68aa3b0b
Jacob Keil <jacobkeil13@gmail.com>
2021-10-07 23:41:43 -0400

    admin update

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,133 +1,163 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const { Webhook, MessageBuilder } = require("discord-webhook-node");
-const convert = require("hex2dec");
-const fetch = require("node-fetch");
-const { findAll, addCurrency } = require("../util-functions/mongodb-functions");
-const connectEnsureLogin = require("connect-ensure-login");
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const convert = require('hex2dec');
+const fetch = require('node-fetch');
+const {
+  findAll,
+  addCurrency,
+  addCreditsAll,
+} = require('../util-functions/mongodb-functions');
+const connectEnsureLogin = require('connect-ensure-login');
 
-const admins = ["195589455430680576", 
-                "545044271389212672",
-                "261717655180804097",
-                "303315367852507136",
-                "356316542423793665"];
+const admins = [
+  '195589455430680576',
+  '545044271389212672',
+  '261717655180804097',
+  '303315367852507136',
+  '356316542423793665',
+];
 
 const redirectHome = (req, res, next) => {
-    if(req.user) {
-      res.redirect('/home');
-    } else { 
-      next();
-    }
-}
+  if (req.user) {
+    res.redirect('/home');
+  } else {
+    next();
+  }
+};
 
 const redirectLogin = (req, res, next) => {
-    if(!req.user) {
-      res.redirect('/auth/discord');
-    } else { 
-      next();
-    }
-}
+  if (!req.user) {
+    res.redirect('/auth/discord');
+  } else {
+    next();
+  }
+};
 
-router.get("/", connectEnsureLogin.ensureLoggedIn({ redirectTo: "/" }), async (req, res) => {
+router.get(
+  '/',
+  connectEnsureLogin.ensureLoggedIn({ redirectTo: '/' }),
+  async (req, res) => {
     let discord;
     let tickets;
 
-    await findAll("users", "tickets").then(t => {
-      tickets = t;
-    }).catch(err => {
-      console.error(err);
-    });
+    await findAll('users', 'tickets')
+      .then((t) => {
+        tickets = t;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-    await findAll("users", "discord").then(d => {
-      discord = d;
-    }).catch(err => {
-      console.error(err);
-    });
+    await findAll('users', 'discord')
+      .then((d) => {
+        discord = d;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-    if (req.user.discordId == "195589455430680576" || 
-        req.user.discordId == "545044271389212672" || 
-        req.user.discordId == "261717655180804097" ||
-        req.user.discordId == "303315367852507136") {
+    if (
+      req.user.discordId == '195589455430680576' ||
+      req.user.discordId == '545044271389212672' ||
+      req.user.discordId == '261717655180804097' ||
+      req.user.discordId == '303315367852507136'
+    ) {
     } else {
-      res.redirect("/home");
+      res.redirect('/home');
       return;
     }
-  
+
     let api_url = `https://discord.com/api/guilds/538558092070092802/channels`;
-  
+
     let discord_ch = await fetch(api_url, {
-      method: "GET", 
+      method: 'GET',
       headers: {
-        "Authorization": `Bot ${process.env.BOT_TOKEN}`
-      }
-    }).then(res => {
-      return res.json();
-    }).then(json => {
-      let channels = [];
-      for(var ch of json) {
-        if (ch.parent_id !== null && ch.type == 0) {
-          channels.push(ch);
+        Authorization: `Bot ${process.env.BOT_TOKEN}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        let channels = [];
+        for (var ch of json) {
+          if (ch.parent_id !== null && ch.type == 0) {
+            channels.push(ch);
+          }
         }
-      }
-  
-      return channels;
-    }).catch(err => {
-      console.log(err);
-    });
-  
-    let profilePic = "";
+
+        return channels;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    let profilePic = '';
     if (req.user.avatar == null) {
-      profilePic = "images/default.png";
+      profilePic = 'images/default.png';
     } else {
       profilePic = `https://cdn.discordapp.com/avatars/${req.user.discordId}/${req.user.avatar}`;
     }
-    const un = req.user.discordTag.split("#");
-    res.render("admin.ejs", {
-        username: un[0],
-        email: req.user.email,
-        avatar: `<img id="user-logo" src="${profilePic}">`,
-        guilds: req.user.guilds,
-        id: req.user.discordId,
-        channels: discord_ch,
-        discord_users: discord,
-        tickets: tickets
+    const un = req.user.discordTag.split('#');
+    res.render('admin.ejs', {
+      username: un[0],
+      email: req.user.email,
+      avatar: `<img id="user-logo" src="${profilePic}">`,
+      guilds: req.user.guilds,
+      id: req.user.discordId,
+      channels: discord_ch,
+      discord_users: discord,
+      tickets: tickets,
     });
-  });
+  }
+);
 
-  router.post("/post/:channel", (req, res) => {
-    let api_url = `https://discord.com/api/channels/${req.params.channel}/messages`;
-  
-    var colorHex = req.body.embed.color;
-    var colorDec = convert.hexToDec(colorHex.replace("#", ""));
-  
-    fetch(api_url, {
-      method: "POST", 
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bot ${process.env.BOT_TOKEN}`
+router.post('/post/:channel', (req, res) => {
+  let api_url = `https://discord.com/api/channels/${req.params.channel}/messages`;
+
+  var colorHex = req.body.embed.color;
+  var colorDec = convert.hexToDec(colorHex.replace('#', ''));
+
+  fetch(api_url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bot ${process.env.BOT_TOKEN}`,
+    },
+    body: JSON.stringify({
+      content: `${req.body.content}`,
+      embed: {
+        title: `${req.body.embed.title}`,
+        description: `${req.body.embed.description}`,
+        color: colorDec,
+        footer: {
+          text: 'Wolfy',
+        },
       },
-      body: JSON.stringify({
-          content: `${req.body.content}`,
-          embed: {
-              title: `${req.body.embed.title}`,
-              description: `${req.body.embed.description}`,
-              color: colorDec,
-              footer: {
-                text: "Wolfy"
-              }
-          }
-      })
-    }).then(res => {
+    }),
+  })
+    .then((res) => {
       return;
       //console.log(res);
-    }).catch(err => {
+    })
+    .catch((err) => {
       console.log(err);
     });
-  });
+});
 
-  router.post("/user/add", async (req, res) => {
-    await addCurrency("users", "discord", req.query.discordId, parseInt(req.query.credits));
-    res.send({ status: "Currency Added" });
-  });
+router.post('/user/add', async (req, res) => {
+  if (req.query.discordId === 'all') {
+    await addCreditsAll(parseInt(req.query.credits));
+    return;
+  }
+  await addCurrency(
+    'users',
+    'discord',
+    req.query.discordId,
+    parseInt(req.query.credits)
+  );
+  res.send({ status: 'Currency Added' });
+});
 
 module.exports = router;
--- src/util-functions/mongodb-functions.js
+++ src/util-functions/mongodb-functions.js
@@ -1,136 +1,174 @@
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGO_DB_URL;
 const mongoClient = new MongoClient(uri);
 
 module.exports = {
-    findAll: async function(db, coll) {
-        let result = [];
-        try {
-            await mongoClient.connect();
-            result = await mongoClient.db(`${db}`).collection(`${coll}`).find().toArray();
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return result;
-    },
-    findOneDiscordId: async function(db, coll, param) {
-        let result;
-        try {
-            await mongoClient.connect();
-            result = await mongoClient.db(`${db}`).collection(`${coll}`).findOne({ discordId: param });
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return result;
-    },
-    addDiscordUser: async function(db, coll, discordId, steamId, user) {
-        try {
-        await mongoClient.connect();
-        await mongoClient.db(`${db}`).collection(`${coll}`).insertOne({ 
-            user: user,
-            discordId: discordId,
-            steamId: steamId,
-            credits: 100
-        });
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return "User Added";
-    },
-    findOneUpdateSteam: async function(db, coll, discordId, steamId, user) {
-        try {
-        await mongoClient.connect();
-        await mongoClient.db(`${db}`).collection(`${coll}`).findOneAndUpdate(
-            { discordId: discordId },
-            { $set: { steamId: steamId, user: user } }
-        );
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return "Steam ID Updated";
-    },
-    findOneTicket: async function(db, coll, param) {
-        let result;
-        try {
-            await mongoClient.connect();
-        result = await mongoClient.db(`${db}`).collection(`${coll}`).findOne({ ticketId: param });
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return result;
-    },
-    findTicketsById: async function (db, coll, param) {
-        let result;
-        try {
-            await mongoClient.connect();
-            result = await mongoClient.db(`${db}`).collection(`${coll}`).find({ openedId: param }).toArray();
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return result;
-    },
-    updateCurrency: async function (db, coll, param, curr) {
-        try {
-        await mongoClient.connect();
-        await mongoClient.db(`${db}`).collection(`${coll}`).findOneAndUpdate(
-            { discordId: param }, 
-            { $inc: { credits: -curr } })
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return "Updated Successfully";
-    },
-    addCurrency: async function (db, coll, param, curr) {
-        try {
-        await mongoClient.connect();
-        await mongoClient.db(`${db}`).collection(`${coll}`).findOneAndUpdate(
-            { discordId: param }, 
-            { $inc: { credits: curr } })
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return "Added Successfully";
-    },
-    findOneQR: async function (param) {
-        let result;
-        try {
-        await mongoClient.connect();
-        result = await mongoClient.db(`users`).collection(`qr`).findOne({ id: param })
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return result;
-    },
-    updateQR: async function (param) {
-        try {
-        await mongoClient.connect();
-        await mongoClient.db(`users`).collection(`qr`).findOneAndUpdate(
-            { id: param }, {$inc: { uses: -1 }});
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await mongoClient.close();
-        }
-        return "Updated QR Uses";
+  findAll: async function (db, coll) {
+    let result = [];
+    try {
+      await mongoClient.connect();
+      result = await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .find()
+        .toArray();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
     }
-}
+    return result;
+  },
+  findOneDiscordId: async function (db, coll, param) {
+    let result;
+    try {
+      await mongoClient.connect();
+      result = await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .findOne({ discordId: param });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return result;
+  },
+  addDiscordUser: async function (db, coll, discordId, steamId, user) {
+    try {
+      await mongoClient.connect();
+      await mongoClient.db(`${db}`).collection(`${coll}`).insertOne({
+        user: user,
+        discordId: discordId,
+        steamId: steamId,
+        credits: 100,
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return 'User Added';
+  },
+  findOneUpdateSteam: async function (db, coll, discordId, steamId, user) {
+    try {
+      await mongoClient.connect();
+      await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .findOneAndUpdate(
+          { discordId: discordId },
+          { $set: { steamId: steamId, user: user } }
+        );
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return 'Steam ID Updated';
+  },
+  findOneTicket: async function (db, coll, param) {
+    let result;
+    try {
+      await mongoClient.connect();
+      result = await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .findOne({ ticketId: param });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return result;
+  },
+  findTicketsById: async function (db, coll, param) {
+    let result;
+    try {
+      await mongoClient.connect();
+      result = await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .find({ openedId: param })
+        .toArray();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return result;
+  },
+  updateCurrency: async function (db, coll, param, curr) {
+    try {
+      await mongoClient.connect();
+      await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .findOneAndUpdate({ discordId: param }, { $inc: { credits: -curr } });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return 'Updated Successfully';
+  },
+  addCurrency: async function (db, coll, param, curr) {
+    try {
+      await mongoClient.connect();
+      await mongoClient
+        .db(`${db}`)
+        .collection(`${coll}`)
+        .findOneAndUpdate({ discordId: param }, { $inc: { credits: curr } });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return 'Added Successfully';
+  },
+  addCreditsAll: async function (curr) {
+    try {
+      await mongoClient.connect();
+      await mongoClient
+        .db(`users`)
+        .collection(`discord`)
+        .updateMany({}, { $inc: { credits: curr } });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return 'Add Credits to All Users';
+  },
+  findOneQR: async function (param) {
+    let result;
+    try {
+      await mongoClient.connect();
+      result = await mongoClient
+        .db(`users`)
+        .collection(`qr`)
+        .findOne({ id: param });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return result;
+  },
+  updateQR: async function (param) {
+    try {
+      await mongoClient.connect();
+      await mongoClient
+        .db(`users`)
+        .collection(`qr`)
+        .findOneAndUpdate({ id: param }, { $inc: { uses: -1 } });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await mongoClient.close();
+    }
+    return 'Updated QR Uses';
+  },
+};
